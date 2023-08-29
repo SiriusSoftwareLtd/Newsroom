463121bcdb2afe42ed73539dc41f74bbaf4241c7
shlex <77512805+shlexware@users.noreply.github.com>
2023-08-29 17:50:39 +0100

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Sirius Newsroom";
-export const SITE_DESCRIPTION = "Sirius Newsroom is the source for news about Sirius. Read press releases, get updates, watch video's and download images.";
+export const SITE_DESCRIPTION = "Sirius Newsroom is the source for news about Sirius. Read press releases, get updates, everything Sirius.";
 export const SITE_URL = "https://blog.sirius.menu";
